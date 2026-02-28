1317f3466f3f9392519a8544c4277faa1d11fd8d
pdh0128 <pdh0128a@gmail.com>
2026-02-28 14:31:11 +0900

    fix(router): 순환참조오류 해결

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 
-from src.api.exception_handler import register_exception_handlers
-from src.api.routers import register_routers
-from src.api.lifespan import lifespan
-
 
 def create_app():
+    from src.api.exception_handler import register_exception_handlers
+    from src.api.routers import register_routers
+    from src.api.lifespan import lifespan
+
     app = FastAPI(lifespan=lifespan)
 
     # 예외 핸들러 등록
--- src/api/v1/app/__init__.py
+++ src/api/v1/app/__init__.py
@@ -1,5 +1,1 @@
 """App API 모듈"""
-
-from .endpoint import app_router
-
-__all__ = ["app_router"]
