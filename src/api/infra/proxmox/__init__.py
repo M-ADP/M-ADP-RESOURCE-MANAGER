45128ee2e5f5e49ff0033ad8b1ae0a454367f7ab
pdh0128 <pdh0128a@gmail.com>
2026-03-02 23:47:46 +0900

    delete(k8s): proxmox 접근 제거

--- src/api/infra/proxmox/__init__.py
+++
@@ -1,3 +0,0 @@
-from src.api.infra.proxmox.endpoint import proxmox_router
-
-__all__ = ["proxmox_router"]
--- src/common/config/__init__.py
+++ src/common/config/__init__.py
@@ -1,11 +1,9 @@
 from src.common.config.kubernetes import KubernetesConfig
 from src.common.config.logger import LoggerConfig
 from src.common.config.monitoring import MonitoringConfig
-from src.common.config.proxmox import PROXMOX_CONFIG
 from src.common.config.vault import VAULT_CONFIG
 
 __all__ = [
-    "PROXMOX_CONFIG", "VAULT_CONFIG",
     "LoggerConfig", "KubernetesConfig", "MonitoringConfig"
 ]
 
