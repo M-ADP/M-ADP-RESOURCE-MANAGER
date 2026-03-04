19f2c03124a797d921122c6d5539133b2098b961
pdh0128 <pdh0128a@gmail.com>
2026-03-04 20:29:23 +0900

    feat(health): health check ㅊㅜ가

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
-import uvicorn
+from http import HTTPStatus
 
+import uvicorn
 from src.api import create_app
 
 app = create_app()
+
+@app.get(status_code=HTTPStatus.ACCEPTED)
+async def health_check():
+    return
 
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=8001)
