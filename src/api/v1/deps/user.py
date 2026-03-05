34e125152b99962de3aa34304a1faa3906300083
pdh0128 <pdh0128a@gmail.com>
2026-03-05 19:41:13 +0900

    refactor(user): user-id -> X-User-Id로 수정

--- src/api/v1/deps/user.py
+++ src/api/v1/deps/user.py
@@ -3,5 +3,5 @@ from fastapi import Header
 from src.core.user.model import User
 
 
-def get_user(user_id: str = Header(alias="user-id")) -> User:
+def get_user(user_id: str = Header(alias="X-User-Id")) -> User:
     return User(id=user_id)
