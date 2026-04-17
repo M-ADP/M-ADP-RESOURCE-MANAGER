927d2f740beff7a08ec2897a7556dd7f80ac787d
박동현 <pdh0128a@gmail.com>
2026-04-17 10:52:31 +0900

    fix(Jenkins): 문법 오류

--- src/dependencies/jenkins.py
+++ src/dependencies/jenkins.py
@@ -1,20 +1,19 @@
 from typing import Optional
-from src.common.config.jenkins import JenkinsConfig
 from src.infra.jenkins.client import JenkinsClient
-from src.core.logger import get_logger, Logger
 
 _jenkins_client_instance: Optional[JenkinsClient] = None
 
 
-def get_jenkins_client(
-    config: Optional[JenkinsConfig] = None, logger: Optional[Logger] = None
-) -> JenkinsClient:
-    """Jenkins 클라이언트 인스턴스 반환"""
+def get_jenkins_client() -> JenkinsClient:
+    """Jenkins 클라이언트 인스턴스 반환 (FastAPI Dependency용)"""
     global _jenkins_client_instance
 
     if _jenkins_client_instance is None:
-        cfg = config or JenkinsConfig()
-        log = logger or get_logger()
-        _jenkins_client_instance = JenkinsClient(cfg, log)
+        from src.common.config.jenkins import JenkinsConfig
+        from src.core.logger import get_logger
+
+        config = JenkinsConfig()
+        logger = get_logger()
+        _jenkins_client_instance = JenkinsClient(config, logger)
 
     return _jenkins_client_instance
